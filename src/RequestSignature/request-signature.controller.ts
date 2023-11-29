a7992014a4c0186a0c12b8259965f3c3790224c9
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-11-29 14:41:07 -0300

    Fix request signature

--- src/RequestSignature/request-signature.controller.ts
+++ src/RequestSignature/request-signature.controller.ts
@@ -3,8 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { ApiParam, ApiCreatedResponse} from '@nestjs/swagger'
 import { RequestSignatureService } from './request-signature.service';
 import { EstablishDto } from '../Dtos/establish.dto';
-import { CustomerDto } from 'src/Dtos/customer.dto';
-import { AddressDto } from 'src/Dtos/address.dto';
+import { normalizeEstablishData } from './request-signature.utils';
+
 
 @Controller()
 export class RequestSignatureController {
@@ -21,18 +21,8 @@ export class RequestSignatureController {
       let rawBody = req.rawBody;
 
       if (rawBody){
-        let json = JSON.parse(rawBody.toString());
 
-        if(json['customer.name']){
-          let address = new AddressDto();
-          address.country = json['customer.address.country'];
-  
-          let customer = new CustomerDto();
-          customer.name = json['customer.name'];
-          customer.address = address;
-  
-          establish.customer = customer;
-        }
+        establish = normalizeEstablishData(establish, rawBody.toString());
         
       }
 
