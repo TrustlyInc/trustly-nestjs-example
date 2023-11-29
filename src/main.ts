07268071dad3933df341606253ee38b5c7ec28de
Renato DelPupo <16341269+renatodelpupo@users.noreply.github.com>
2023-11-29 13:51:02 -0300

    Fix CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
     rawBody: true,
   });
 
