31d6ddad01dfcf76cce4fa3b82f3bc82a36f1f51
Renato DelPupo <16341269+renatodelpupo@users.noreply.github.com>
2023-12-01 11:12:48 -0300

    Format document

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
   Alpha Merchant App - Simple Backend
 </p>
 
-
 ## Description
 
 Simple backend to demonstrate how to implement the endpoints to integrate with Trustly.
@@ -19,24 +18,29 @@ $ git clone https://github.com/TrustlyInc/trustly-nestjs-example.git
 ```
 
 ## Configuring Trustly API credentials
+
 In the `.development.env` file replace environment variables values:
+
 - ACCESS_ID
 - ACCESS_KEY
 - BASE_URL
 
 ## Install dependencies
+
 ```bash
 # install dependencies
 $ npm install
 ```
 
 ## Running the app
+
 ```bash
 # development
 $ npm run start:dev
 ```
 
 ## Swagger
+
 ```
 http://localhost:8080/api
 ```
--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { HttpModule } from "@nestjs/axios";
+import { HttpModule } from '@nestjs/axios';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            envFilePath: '.development.env'
-          }),
-        HttpModule,
-      ],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
+    HttpModule,
+  ],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
-
 export class TransactionModule {}
-
